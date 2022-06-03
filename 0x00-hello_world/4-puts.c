4328ad2360d8084b293c21329d8a0683a6ee26be
Ogheneruona Ewherido <ruonae@gmail.com>
2022-06-02 20:35:59 -0700

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+i
 int main(void)
 {
-puts ("\"Programming is like building a multillingual puzzle");
+puts("\"Programming is like building a multillingual puzzle");
 return (0);
 }
