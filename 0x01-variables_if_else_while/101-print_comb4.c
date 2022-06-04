6c243a3165521b1ff065933bd46fee4828829b9d
Ogheneruona Ewherido <ruonae@gmail.com>
2022-06-04 14:03:53 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,38 +10,38 @@ int main(void)
 
 {
 
-int a = 0;
-int b = 0;
-int c = 0;
-int coma = 1;
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int coma = 1;
 
-while (a <= 9)
-{
-	while (b <= 9)
+	while (a <= 9)
 	{
-		while (c <= 9)
+		while (b <= 9)
 		{
-			if (a < b && b < c)
-			{
-				if (coma == 0)
-				{
-					putchar(',');
-					putchar(32);
+			while (c <= 9)
+			{	
+				if (a < b && b < c)
+				{	
+					if (coma == 0)
+					{
+						putchar(',');
+						putchar(32);
+					}
+					coma = 0;
+					putchar(a + '0');
+			        	putchar(b + '0');
+					putchar(c + '0');
 				}
-				coma = 0;
-				putchar(a + '0');
-			        putchar(b + '0');
-				putchar(c + '0');
+				++c;
 			}
-			++c;
-		}
-		++b;
-		c = 0;
+			++b;
+			c = 0;
+		}	
+		++a;
+		b = 0;
 	}
-	++a;
-	b = 0;
-}
-putchar(10);
+	putchar(10);
 
 return (0);
 }
