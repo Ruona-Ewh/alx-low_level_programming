45de18aef0f6cc65ae159eb68b628b11a8f3f56f
Ogheneruona Ewherido <ruonae@gmail.com>
2022-06-07 17:06:51 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char string[8] = "_putchar";
+	int n = 0;
+	
+	while (n < 8)
+	{
+		_putchar(string[n]);
+		++n
+	}
+	_putchar(10);
+	return (0);
+}
