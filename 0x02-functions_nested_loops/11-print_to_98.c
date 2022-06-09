c564882528ad479ed45891a5beae8c242218167e
Ogheneruona Ewherido <ruonae@gmail.com>
2022-06-08 21:27:47 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_to_98 - function
+ *
+ * @n - variable
+ *
+ * Return: natural numbers
+ */
+void print_to_98(int n)
+{
+int upper = 98;
+if (n > upper)
+{
+	while (n >= upper)
+	{
+		if (n != upper)
+		{
+			printf("%d%s", n, ", ");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+		n--;
+	}
+}
+else
+{
+	while (n <= upper)
+	{
+		if (n != upper)
+		{
+			printf("%d%s", n, ", ");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+		n++;
+	}
+}
+putchar(10);
+}
